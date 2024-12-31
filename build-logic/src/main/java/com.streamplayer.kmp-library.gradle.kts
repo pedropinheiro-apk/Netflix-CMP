c3c02d49dbc10f785b1d4f5b6f52b483d8b3abde
Rods <rodrigo.vianna.oliveira@gmail.com>
2024-12-31 17:56:00 -0300

    [ISSUE-2] - removing block unnecessary

--- build-logic/src/main/java/com.streamplayer.kmp-library.gradle.kts
+++ build-logic/src/main/java/com.streamplayer.kmp-library.gradle.kts
@@ -29,9 +29,6 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_17)
         }
     }
-    sourceSets.named("androidMain").configure {
-        kotlin.srcDir("build/generated/ksp/metadata/androidMain/kotlin")
-    }
 }
 
 android {
