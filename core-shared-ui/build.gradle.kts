a58631c7bd675964b1c0fa26a85cf00805e4ee23
Rods <rodrigo.vianna.oliveira@gmail.com>
2025-01-15 20:34:43 -0300

    [ISSUE-3] - core-shared-ui to androidMain

--- core-shared-ui/build.gradle.kts
+++ core-shared-ui/build.gradle.kts
@@ -1,21 +1,25 @@
 @file:Suppress("UnstableApiUsage")
 plugins {
-    id("com.streamplayer.android-library")
+    id("com.streamplayer.kmp-library")
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.compose.compiler)
 }
 
-dependencies {
-    implementation(projects.coreShared)
-    implementation(compose.material3)
-    implementation(compose.preview)
-    implementation(compose.ui)
-    implementation(libs.navigation.compose)
-    implementation(libs.bundles.koin)
-    implementation(libs.bundles.kotlin)
-    implementation(libs.bundles.androidSupport)
-    implementation(libs.android.youtube.player)
-    implementation(libs.paging.compose)
-    implementation(libs.coil)
-    testImplementation(libs.bundles.test)
+kotlin {
+    sourceSets {
+        androidMain.dependencies {
+            implementation(projects.coreShared)
+            implementation(compose.material3)
+            implementation(compose.preview)
+            implementation(compose.ui)
+            implementation(libs.navigation.compose)
+            implementation(libs.bundles.koin)
+            implementation(libs.bundles.kotlin)
+            implementation(libs.bundles.androidSupport)
+            implementation(libs.android.youtube.player)
+            implementation(libs.paging.compose)
+            implementation(libs.coil)
+        }
+    }
 }
+
