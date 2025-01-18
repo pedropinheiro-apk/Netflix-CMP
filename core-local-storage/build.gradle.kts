29822f7c22a8fe714413aa7f1a4864721c6da0ec
Rods <rodrigo.vianna.oliveira@gmail.com>
2025-01-18 10:33:14 -0300

    [ISSUE-3] - core-localstorage to androidMain

--- core-local-storage/build.gradle.kts
+++ core-local-storage/build.gradle.kts
@@ -1,13 +1,27 @@
 plugins {
-    id("com.streamplayer.android-library")
+    id("com.streamplayer.kmp-library")
     id("com.google.devtools.ksp")
 }
 
-dependencies {
+kotlin {
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.bundles.room)
+            implementation(libs.bundles.kotlin)
+            implementation(libs.bundles.koin)
+        }
+    }
+}
 
+dependencies {
+    add("kspCommonMainMetadata",libs.koin.compiler)
+    add("kspAndroid", libs.koin.compiler)
+}
+
+ksp {
+    arg("KOIN_CONFIG_CHECK","true")
+}
+
+dependencies {
     ksp(libs.roomCompiler)
-    implementation(libs.bundles.room)
-    implementation(libs.bundles.kotlin)
-    implementation(libs.bundles.koin)
-    testImplementation(libs.bundles.test)
 }
