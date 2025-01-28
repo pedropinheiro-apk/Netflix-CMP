5f33c4b19251d1b7ba01f1d01273ca179427b555
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-01-28 08:00:54 -0300

    Migrate paging library to KMP

--- feature-detail/build.gradle.kts
+++ feature-detail/build.gradle.kts
@@ -21,7 +21,6 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.preview)
-            implementation(libs.paging.compose)
             implementation(libs.navigation.compose)
             implementation(libs.bundles.koin)
             implementation(libs.bundles.networking)
--- feature-search/build.gradle.kts
+++ feature-search/build.gradle.kts
@@ -21,20 +21,20 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.preview)
-            implementation(libs.paging.compose)
             implementation(libs.navigation.compose)
             implementation(libs.bundles.koin)
             implementation(libs.bundles.networking)
             implementation(libs.coil)
             implementation(libs.koin.annotations)
             implementation(libs.bundles.androidSupport)
+            implementation(libs.paging.compose)
         }
     }
 }
 
 dependencies {
-    add("kspCommonMainMetadata",libs.koin.compiler)
-    add("kspAndroid", libs.koin.compiler)
+    add("kspCommonMainMetadata",libs.koin.ksp.compiler)
+    add("kspAndroid", libs.koin.ksp.compiler)
 }
 
 ksp {
