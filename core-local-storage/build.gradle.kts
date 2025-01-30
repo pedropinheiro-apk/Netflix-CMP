eaa33fbfc9c4d53ab4b6edbc782f25179b6b032e
Rods <rodrigo.vianna.oliveira@gmail.com>
2025-01-30 12:58:44 -0300

    [ISSUE-6] - local-storage and navigation AndroidMain to commonMain

--- core-local-storage/build.gradle.kts
+++ core-local-storage/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 kotlin {
     sourceSets {
-        androidMain.dependencies {
+        commonMain.dependencies {
             implementation(libs.bundles.room)
             implementation(libs.bundles.kotlin)
             implementation(libs.bundles.koin)
