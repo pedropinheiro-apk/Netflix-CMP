1586a46d0846f2c55b5697b4703c1fcac8d17f6e
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-02-25 07:58:04 -0300

    Lottie animation for iOS - compatible multiplatform way

---
+++ iosApp/iosApp/Lottie/Lottie.swift
@@ -0,0 +1,59 @@
+//
+//  Lottie.swift
+//  iosApp
+//
+//  Created by Gabriel Bronzatti Moro on 25/02/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import Lottie
+import UIKit
+import streamplayerapp
+
+class LottieViewProviderImpl : LottieViewProvider {
+    func provideLottieView(lottieAnimationJson: String) -> UIView {
+        let lottieView = LottieView()
+        lottieView.setupLottieAnimationView(
+            animationContentJson: lottieAnimationJson
+        )
+        return lottieView
+    }
+}
+
+class LottieView : UIView {
+    
+    private let animationView: LottieAnimationView
+    
+    override init(frame: CGRect) {
+        animationView = LottieAnimationView()
+
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+    
+    func setupLottieAnimationView(animationContentJson: String) {
+        
+        animationView.contentMode = .scaleAspectFit
+        
+        addSubview(animationView)
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+
+        animationView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        animationView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        animationView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        animationView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        animationView.loopMode = .loop
+        animationView.frame = bounds
+        
+        guard let data = jsonString.data(using: .utf8) else { return nil }
+        let animation = JSONDecoder().decode(LottieAnimation.self, from: data)
+        animationView.animation = animation
+        animationView.loopMode = .loop
+        animationView.play()
+    }
+}
