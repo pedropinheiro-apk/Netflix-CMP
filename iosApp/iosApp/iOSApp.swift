0b0fc4b2e8f6f1b89e9c6876594ed7d2cb6443f2
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2025-02-25 21:41:20 -0300

    initializing koin in ios app

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import streamplayerapp
 
 @main
 struct iOSApp: App {
+    
+    init() {
+        KoinIosHelper().doInitKoin()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
