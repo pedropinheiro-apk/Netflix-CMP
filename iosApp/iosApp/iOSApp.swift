4916bf9523cb22476d7a8dc60077c0ac87430428
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2025-05-12 22:32:38 -0300

    configured firebase for ios

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 import streamplayerapp
+import FirebaseCore
 
 @main
 struct iOSApp: App {
     
     init() {
         KoinIosHelper().doInitKoin(lottieViewProvider: LottieViewProviderImpl())
+        FirebaseApp.configure()
     }
     
     var body: some Scene {
