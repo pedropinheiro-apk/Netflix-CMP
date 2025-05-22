805c02531fd700021698b718bc814c25c9683480
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-05-22 19:20:07 -0300

    Refactor: Update PopcornPlugin configuration
    This commit refactors the PopcornPlugin configuration in `popcorngp-setup-plugin.gradle.kts`.
    
    The following changes were made:
    
    - Added new rules for `:core-shared-ui` to only depend on `:core-shared`.
    - Added new rules for `:core-shared` to have no dependencies.
    - Updated existing rules for feature modules to ensure they do not depend on each other.

--- build-logic/src/main/java/popcorngp-setup-plugin.gradle.kts
+++ build-logic/src/main/java/popcorngp-setup-plugin.gradle.kts
@@ -1,6 +1,8 @@
 import com.github.codandotv.popcorn.domain.input.PopcornChildConfiguration
 import com.github.codandotv.popcorn.domain.input.ProjectType
 import com.github.codandotv.popcorn.domain.rules.DoNotWithRule
+import com.github.codandotv.popcorn.domain.rules.JustWithRule
+import com.github.codandotv.popcorn.domain.rules.NoDependencyRule
 
 plugins {
     id("io.github.codandotv.popcorngpparent")
@@ -10,6 +12,29 @@ popcornGuineapigParentConfig {
     type = ProjectType.KMP
 
     children = listOf(
+        /**
+         * core-shared-ui should only depends on core-shared
+         */
+        PopcornChildConfiguration(
+            moduleNameRegex = ":core-shared-ui",
+            rules = listOf(
+                JustWithRule(
+                    justWith = listOf("core-shared")
+                )
+            ),
+        ),
+        /**
+         * core-shared should not have dependencies
+         */
+        PopcornChildConfiguration(
+            moduleNameRegex = ":core-shared",
+            rules = listOf(
+                NoDependencyRule()
+            ),
+        ),
+        /**
+         * feature modules should not depend on each other
+         */
         PopcornChildConfiguration(
             moduleNameRegex = ":feature-[a-z]+",
             rules = listOf(
